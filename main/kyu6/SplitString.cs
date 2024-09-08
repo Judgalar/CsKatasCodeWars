c6eacf68f330884a299db8fa42d0b6fe7846653c
Judgalar <judgalar@gmail.com>
2024-09-08 13:43:56 +0200

    SplitString

---
+++ main/kyu6/SplitString.cs
@@ -0,0 +1,31 @@
+﻿/*
+Complete the solution so that it splits the string into pairs of two characters. 
+If the string contains an odd number of characters then it should replace the missing second character of the final pair  with an underscore ('_').
+
+Examples:
+    * 'abc' =>  ['ab', 'c_']
+    * 'abcdef' => ['ab', 'cd', 'ef']
+*/
+
+using System.Text;
+
+namespace kata.kyu6;
+
+public class SplitString
+{
+    public static string[] Solution(string str)
+    {
+        string s = (str.Length % 2 == 0) ? str : str + "_";
+
+        string[] resul = new string[s.Length/2];
+
+        int j = 0;
+        for (int i = 0; i < s.Length;)
+        {
+            resul[j++] = s[i++].ToString() + s[i++].ToString();
+        }
+
+        return resul;
+    }
+}
+
---
+++ test/kyu6/SplitStringTests.cs
@@ -0,0 +1,19 @@
+﻿using kata.kyu6;
+using NUnit.Framework;
+using System;
+
+namespace test.kyu6;
+
+[TestFixture]
+public class SplitStringTests
+{
+    [Test]
+    public void BasicTests()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(SplitString.Solution("abc"), Is.EqualTo(new string[] { "ab", "c_" }));
+            Assert.That(SplitString.Solution("abcdef"), Is.EqualTo(new string[] { "ab", "cd", "ef" }));
+        });
+    }
+}
